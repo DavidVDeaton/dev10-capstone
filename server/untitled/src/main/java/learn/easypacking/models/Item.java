951d139e1c91f4e3a869cf51e984c99d9e2aa5ba
IvanWhoDis <Ivangaoyuan@Gmail.com>
2023-03-26 18:06:31 -0400

    data layer for container and item

---
+++ server/untitled/src/main/java/learn/easypacking/models/Item.java
@@ -0,0 +1,73 @@
+package learn.easypacking.models;
+
+public class Item {
+    private int itemId;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public boolean isPackStatus() {
+        return packStatus;
+    }
+
+    public void setPackStatus(boolean packStatus) {
+        this.packStatus = packStatus;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getContainerId() {
+        return containerId;
+    }
+
+    public void setContainerId(int containerId) {
+        this.containerId = containerId;
+    }
+
+    private String itemName;
+
+    private boolean packStatus;
+
+    private int quantity;
+
+    private String description;
+
+    private int userId;
+
+    private int containerId;
+}
